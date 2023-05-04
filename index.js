ad4fd82213e2cd6a8e06cf9b22297298b64932b6
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2023-05-04 15:47:20 -0500

    '/location'

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+const zip_codes = process.env.ZIPKEY
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// listen on the port
+app.get('/location', (req, res) => {
+    const zip = req.body.zipcode;
+    const country = req.body.country;
+    const headers = {
+        "apikey": zip_codes,
+        "Accept": "application/json"
+    }
+
+    axios.get(`https://api.zipcodestack.com/v1/search?codes=${zip}&country=${country}`, {
+        headers
+    }).then((response) => {
+        const val = Object.values(response.data.results)[0];
+        console.log(val[0].longitude, val[0].latitude)
+        res.status(201).json(
+            {
+                longitude: val[0].longitude, 
+                latitude: val[0].latitude
+            });
+    }).catch((error) => {
+        // console.log(error);
+        res.status(500).send(error);
+    });
+
+})
+
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
